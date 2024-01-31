2943c74d0abd2c66e053018b59ba7f5a2b686223
Niklas Röttger <niklas.roettger@outlook.de>
2024-01-31 10:54:35 +0100

    add session code

--- index.js
+++ index.js
@@ -3,28 +3,54 @@ console.clear();
 /* -------- Destructuring Assignment - Objects --------- */
 
 const tree = {
-  name: "oak",
+  name: 'oak',
   age: 1200,
-  scientificName: "quercus",
+  scientificName: 'quercus',
 };
+
+// const name = tree.name;
+// const age = tree.age;
+// const scientificName = tree.scientificName;
+
+// const {
+//   name: colloquialName,
+//   age,
+//   scientificName,
+//   keepLeavesInWinter = false,
+// } = tree;
+
+const { age, ...information } = tree;
+
+// console.log(information);
+// console.log(tree);
 
 /* -------- Destructuring Assignment - Arrays --------- */
 
-const animals = ["Lion", "Tiger", "Penguin", "Bear", "Whale"];
+const animals = ['Lion', 'Tiger', 'Penguin', 'Bear', 'Whale'];
+
+const [lion, , penguin, bear, ...whale] = animals;
+
+// console.log(whale);
 
 /* ---------------- Rest Syntax ----------------- */
 
-// function showZooAnimals(allAnimals) {
-// 	console.log(allAnimals);
-// 	return `My zoo consists of the following animals: ${allAnimals.join(', ')}.`;
-// }
+function showZooAnimals(...allAnimals) {
+  console.log(allAnimals);
+  return `My zoo consists of the following animals: ${allAnimals.join(', ')}.`;
+}
+
+// console.log(showZooAnimals('Monkey', 'Crocodile', 'Pig'));
 
 /* ---------------- Spread Syntax ----------------- */
 
 // Add another value
 
-const animalFood = ["Meat", "Apple", "Banana", "Carrot"];
+const animalFood = ['Meat', 'Apple', 'Banana', 'Carrot'];
 
-const newAnimalFood = "Fish";
+const newAnimalFood = 'Fish';
+
+const allAnimalFood = [newAnimalFood, ...animalFood];
+
+// console.log(allAnimalFood);
 
 // Spread for concatenating two arrays
