b345fd654ca44428a79e712ab3cab13b47da87e1
Niklas Röttger <niklas.roettger@outlook.de>
2024-01-31 10:58:30 +0100

    add comments

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ const tree = {
 // const scientificName = tree.scientificName;
 
 // const {
-//   name: colloquialName,
+//   name: colloquialName, -> rename the variable
 //   age,
 //   scientificName,
-//   keepLeavesInWinter = false,
+//   keepLeavesInWinter = false, -> assign a default value
 // } = tree;
 
-const { age, ...information } = tree;
+const { age, ...information } = tree; // saving all keys apart from age in the information object
 
 // console.log(information);
 // console.log(tree);
@@ -28,13 +28,14 @@ const { age, ...information } = tree;
 
 const animals = ['Lion', 'Tiger', 'Penguin', 'Bear', 'Whale'];
 
-const [lion, , penguin, bear, ...whale] = animals;
+const [lion, , penguin, bear, ...whale] = animals; // skip the second entry and save all entries after the fourth in the whale array
 
 // console.log(whale);
 
 /* ---------------- Rest Syntax ----------------- */
 
 function showZooAnimals(...allAnimals) {
+  // use rest syntax to always create an array allAnimals with all arguments being passed in
   console.log(allAnimals);
   return `My zoo consists of the following animals: ${allAnimals.join(', ')}.`;
 }
@@ -49,7 +50,7 @@ const animalFood = ['Meat', 'Apple', 'Banana', 'Carrot'];
 
 const newAnimalFood = 'Fish';
 
-const allAnimalFood = [newAnimalFood, ...animalFood];
+const allAnimalFood = [newAnimalFood, ...animalFood]; // spread animalFood array to a new array
 
 // console.log(allAnimalFood);
 
